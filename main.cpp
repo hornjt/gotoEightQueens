c75b9647e327413e116e8db7ab28978d84f9ede7
Jon Horn <jhorn85@gmail.com>
2017-02-17 18:10:30 -0500

    first commit; nothing added

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gotoEightQueens
+//
+//  Created by Jon Horn on 2/17/17.
+//  Copyright © 2017 Jon Horn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
