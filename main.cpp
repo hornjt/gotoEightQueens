64664646864b71d8d93321f741933c56e93b3f1b
Jon Horn <jhorn85@gmail.com>
2017-02-17 19:59:10 -0500

    Endless loop....

--- main.cpp
+++ main.cpp
@@ -20,7 +20,8 @@
 
 int main() {
     int board[8][8] = {0}, r = -1, c = 0, i = 0;
-    
+
+    board[0][0] = 1;
     for (int j = 0; j < 8; j++) {
         for(int k = 0; k < 8; k++) {
             std::cout << board[j][k] << " ";
@@ -28,9 +29,6 @@ int main() {
         std::cout << std::endl;
     }
     std::cout << std::endl;
-    
-//    print();
-    board[0][0] = 1;
 nc:
     c++;
     if (c == 8) {
@@ -42,26 +40,29 @@ nr:
     if (r == 8)
         goto backtrack;
     for (i = 0; i < c; i++) { // row test
-        if (board[r][i] == 0)
+        if (board[r][i] == 1)
             goto nr;
     }
     for (i = 0; i < r; i++) { // column test
-        if (board[i][c] == 0)
+        if (board[i][c] == 1)
             goto nc;
     }
-    for (i = 1; r >= 0 && c >= 0; i++) { // up diagonal
-        if (board[r - i][c - i] == 0)
+    for (i = 1; r - i >= 0 && c - i >= 0; i++) { // up diagonal
+        if (board[r - i][c - i] == 1)
             goto nr;
     }
-    for (i = 1; r < 8 && c >= 0; i++) { // down diagonal
-        if (board[r + i][r - i] == 0)
+    for (i = 1; i < 8; i++) { // down diagonal
+        if (board[r + i][c - i] == 1)
             goto nr;
     }
     board[r][c] = 1;
     goto nc;
 backtrack:
     c--;
-    if (c == -1) return 0;
+    if (c == -1) {
+        std::cout << "hit c = -1";
+        return 0;
+    }
     r = 0;
     while (board[r][c] != 1) {
         board[r][c] = 0;
